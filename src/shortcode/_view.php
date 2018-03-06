b7930043d925ef184f8206c6ef84ec5c72364c0b
Takashi Kitajima <inc@2inc.org>
2018-03-06 13:49:49 +0900

    Add action hook inc2734_wp_contents_outline_before

--- src/shortcode/_view.php
+++ src/shortcode/_view.php
@@ -10,21 +10,27 @@ if ( empty( $attributes['headings'] ) ) {
 }
 ?>
 
-<div class="wpco" aria-hidden="true" id="<?php echo esc_attr( $wpco_id ); ?>">
+<div class="wpco-wrapper" aria-hidden="true" id="<?php echo esc_attr( $wpco_id ); ?>">
 	<?php
-	$move_to = 'true';
-	if ( isset( $attributes['move_to_before_1st_heading'] ) && 'false' === $attributes['move_to_before_1st_heading'] ) {
-		$move_to = 'false';
-	}
+	do_action( 'inc2734_wp_contents_outline_before', $attributes );
 	?>
-	<script>
-	jQuery(function($) {
-		$('#<?php echo esc_js( $wpco_id ); ?>').contentsOutline({
-			headings: $('<?php echo esc_js( $post_class ); ?> <?php echo esc_js( $attributes['selector'] ); ?>').children('<?php echo esc_js( $attributes['headings'] ); ?>'),
-			moveToBefore1stHeading: <?php echo esc_js( $move_to ); ?>
+
+	<div class="wpco">
+		<?php
+		$move_to = 'true';
+		if ( isset( $attributes['move_to_before_1st_heading'] ) && 'false' === $attributes['move_to_before_1st_heading'] ) {
+			$move_to = 'false';
+		}
+		?>
+		<script>
+		jQuery(function($) {
+			$('#<?php echo esc_js( $wpco_id ); ?>').contentsOutline({
+				headings: $('<?php echo esc_js( $post_class ); ?> <?php echo esc_js( $attributes['selector'] ); ?>').children('<?php echo esc_js( $attributes['headings'] ); ?>'),
+				moveToBefore1stHeading: <?php echo esc_js( $move_to ); ?>
+			});
 		});
-	});
-	</script>
-	<h2 class="wpco__title"><?php esc_html_e( 'Outline', 'inc2734-wp-contents-outline' ); ?></h2>
-	<div class="contents-outline"></div>
+		</script>
+		<h2 class="wpco__title"><?php esc_html_e( 'Outline', 'inc2734-wp-contents-outline' ); ?></h2>
+		<div class="contents-outline"></div>
+	</div>
 </div>
