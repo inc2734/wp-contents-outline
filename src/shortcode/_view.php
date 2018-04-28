bb6a575ef1739a22528f6f0619e9cfb506706085
Takashi Kitajima <inc@2inc.org>
2018-04-28 16:17:35 +0900

    Refactoring

--- src/shortcode/_view.php
+++ src/shortcode/_view.php
@@ -8,6 +8,23 @@
 if ( empty( $attributes['headings'] ) ) {
 	return;
 }
+
+add_action( 'wp_footer', function() use ( $attributes, $post_class, $wpco_id ) {
+	$move_to = 'true';
+	if ( isset( $attributes['move_to_before_1st_heading'] ) && 'false' === $attributes['move_to_before_1st_heading'] ) {
+		$move_to = 'false';
+	}
+	?>
+<script>
+jQuery(function($) {
+	$('#<?php echo esc_js( $wpco_id ); ?>').contentsOutline({
+		headings: $('<?php echo esc_js( $post_class ); ?> <?php echo esc_js( $attributes['selector'] ); ?>').children('<?php echo esc_js( $attributes['headings'] ); ?>'),
+		moveToBefore1stHeading: <?php echo esc_js( $move_to ); ?>
+	});
+});
+</script>
+	<?php
+}, 9999 );
 ?>
 
 <div class="wpco-wrapper" aria-hidden="true" id="<?php echo esc_attr( $wpco_id ); ?>">
@@ -16,20 +33,6 @@ if ( empty( $attributes['headings'] ) ) {
 	?>
 
 	<div class="wpco">
-		<?php
-		$move_to = 'true';
-		if ( isset( $attributes['move_to_before_1st_heading'] ) && 'false' === $attributes['move_to_before_1st_heading'] ) {
-			$move_to = 'false';
-		}
-		?>
-		<script defer>
-		jQuery(function($) {
-			$('#<?php echo esc_js( $wpco_id ); ?>').contentsOutline({
-				headings: $('<?php echo esc_js( $post_class ); ?> <?php echo esc_js( $attributes['selector'] ); ?>').children('<?php echo esc_js( $attributes['headings'] ); ?>'),
-				moveToBefore1stHeading: <?php echo esc_js( $move_to ); ?>
-			});
-		});
-		</script>
 		<h2 class="wpco__title"><?php esc_html_e( 'Outline', 'inc2734-wp-contents-outline' ); ?></h2>
 		<div class="contents-outline"></div>
 	</div>
