aebe90d61d678b4335e0a1ea8165c9a6949405e2
Takashi Kitajima <inc@2inc.org>
2018-10-29 15:27:15 +0900

    Some fixes

--- src/shortcode/shortcode.php
+++ src/shortcode/shortcode.php
@@ -11,33 +11,39 @@
  *        @var int post_id
  * @return void
  */
-add_shortcode( 'wp_contents_outline', function( array $attributes = array() ) {
-	if ( ! is_singular() ) {
-		return;
+add_shortcode(
+	'wp_contents_outline',
+	function( array $attributes = array() ) {
+		if ( ! is_singular() ) {
+			return;
+		}
+
+		$attributes = shortcode_atts(
+			[
+				'selector'                   => null,
+				'headings'                   => 'h2,h3,h4,h5,h6',
+				'post_id'                    => get_the_ID(),
+				'move_to_before_1st_heading' => 'false',
+			],
+			$attributes
+		);
+
+		if ( empty( $attributes['post_id'] ) ) {
+			return;
+		}
+
+		$_post = get_post( $attributes['post_id'] );
+		if ( ! $_post ) {
+			return;
+		}
+
+		$post_id    = $_post->ID;
+		$post_type  = $_post->post_type;
+		$post_class = '.post-' . $post_id;
+		$wpco_id    = 'wpco-' . uniqid( rand() );
+
+		ob_start();
+		include( __DIR__ . '/_view.php' );
+		return ob_get_clean();
 	}
-
-	$attributes = shortcode_atts( [
-		'selector'                   => null,
-		'headings'                   => 'h2,h3,h4,h5,h6',
-		'post_id'                    => get_the_ID(),
-		'move_to_before_1st_heading' => 'false',
-	], $attributes );
-
-	if ( empty( $attributes['post_id'] ) ) {
-		return;
-	}
-
-	$_post = get_post( $attributes['post_id'] );
-	if ( ! $_post ) {
-		return;
-	}
-
-	$post_id    = $_post->ID;
-	$post_type  = $_post->post_type;
-	$post_class = '.post-' . $post_id;
-	$wpco_id    = 'wpco-' . uniqid( rand() );
-
-	ob_start();
-	include( __DIR__ . '/_view.php' );
-	return ob_get_clean();
-} );
+);
